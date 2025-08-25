e0d6580d1dd59e91f5c0fa808d719bb00dcbf615
Mubarek Ahmed Mohammed <mubarekahmed349@gmail.com>
2025-08-26 00:07:08 +0300

    fixing /prisma/generated/client

--- lib/db.ts
+++ lib/db.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '../app/generated/prisma';
+import { PrismaClient } from '../prisma/generated/client';
 
 // Extend global type for Prisma Client singleton
 declare global {
